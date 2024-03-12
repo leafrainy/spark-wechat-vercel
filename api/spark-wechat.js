15f2f330a26a45d8fc0145e21ae1f30e49946934
leafrainy <825591305@qq.com>
2024-03-12 20:33:17 +0800

    Update spark-wechat.js

--- api/spark-wechat.js
+++ api/spark-wechat.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 const url = require('url');
 const querystring = require('querystring');
 const xml2js = require('xml2js');
-const WebSocket = require('ws');
 const PocketBase = require('pocketbase/cjs');
 
 dotenv.config();
@@ -88,7 +87,7 @@ const emojiObj = {
 };
 const pb = new PocketBase(process.env.PB);
 const payUrl = process.env.PAYURL;
-const aiUrl = process.env.AIURL;
+const linkUrl = process.env.LINKURL;
 const keywordAutoReply = JSON.parse(process.env.KEYWORD_REPLAY);
 module.exports = async function (request, response) {
   const method = request.method;
@@ -130,8 +129,8 @@ module.exports = async function (request, response) {
       Content = '我发送了表情：' + emojiObj[Content] + '，现在你要怎么做'
     }
     //关键词触发
-    if(Content==="充值"){
-      const pay_msg = `当前系统仅支持单次充值一元人民币（两次生成机会），如果同意请<a href="https://${payUrl}?uid=${FromUserName}">点击此处前往充值</a>`;
+    if(Content==="会员"){
+      const pay_msg = `当前系统仅支持单次1.88元人民币（购买永久VIP），如果同意请<a href="https://${payUrl}?uid=${FromUserName}">点击此处前往购买VIP</a>`;
       console.log("触发关键词自动回复");
       response.status(200).send(formatReply(
         FromUserName,
@@ -141,23 +140,9 @@ module.exports = async function (request, response) {
       ));
       return
     }
-    if(Content==="余额"){
-      const record = await pb.collection('ai_user').getFirstListItem('username2="'+FromUserName+'"');
-      console.log(record);
-      const yue = record.num
-      const yue_msg = `您当前剩余生成次数：${yue}次，如果余额不足请<a href="https://${payUrl}?uid=${FromUserName}">点击此处前往充值</a>`;
-      console.log("触发关键词自动回复");
-      response.status(200).send(formatReply(
-        FromUserName,
-        ToUserName,
-        timeNow,
-        yue_msg
-      ));
-      return
-    }
-    if(Content==="ai"){
+    if(Content==="抖音"){
       
-      const ai_msg = `请<a href="https://${aiUrl}?uid=${FromUserName}">点击此处前往AI创作</a>`;
+      const ai_msg = `请<a href="https://${linkUrl}?uid=${FromUserName}">点击此处前往抖音名片制作</a>`;
       console.log("触发关键词自动回复");
       response.status(200).send(formatReply(
         FromUserName,
@@ -192,16 +177,17 @@ module.exports = async function (request, response) {
         //注册用户
         const data = {
           "username": calculateMD5(FromUserName),
-          "username2":FromUserName,
+          "wx_id":FromUserName,
             
           "email": "",
           "emailVisibility": true,
           "password": FromUserName,
           "passwordConfirm": FromUserName,
-          "num": 1
+          "num": 1,
+          "is_vip":0
       };
   
-      await pb.collection('ai_user').create(data);
+      await pb.collection('link_user').create(data);
       }catch(e){
         console.log(e);
       }
@@ -218,111 +204,6 @@ module.exports = async function (request, response) {
       return response.status(200).send('');
     }
   }
-
-  if (userHasAnswerIng[FromUserName]) {
-    response.status(200).send(formatReply(
-      FromUserName,
-      ToUserName,
-      timeNow,
-      '微信规定要在5s内回复，但是本次需要回复的内容很长，现在还没整理好，所以你暂时看到了这条消息。请稍后回复任意文字尝试获取回复。比如数字 1。'
-    ));
-    return;
-  }
-
-  if (userStashMsg[FromUserName]) {
-    console.log('用户有暂存数据，返回暂存数据');
-    let tmp = userStashMsg[FromUserName];
-    userStashMsg[FromUserName] = '';
-    response.status(200).send(formatReply(
-      FromUserName,
-      ToUserName,
-      timeNow,
-      tmp
-    ));
-    return;
-  }
-  console.log("当前时间：", timeNow, "上次时间：", userLastChatTime[FromUserName])
-  if (
-    userLastChatTime[FromUserName] &&
-    timeNow - userLastChatTime[FromUserName] >= 300
-  ) {
-    userChatHistory[FromUserName] = [];
-  }
-  userLastChatTime[FromUserName] = timeNow;
-  if (!userChatHistory[FromUserName]) {
-    userChatHistory[FromUserName] = [];
-  }
-  userChatHistory[FromUserName].push({ Role: 'user', Content });
-  console.log("会话历史：", userChatHistory);
-  const data = genParams(userChatHistory[FromUserName]);
-
-  const connect = await getConnect();
-  connect.send(JSON.stringify(data));
-
-  let answer = '';
-  let timeout;
-  const done = new Promise((resolve) => {
-    connect.on('message', (msg) => {
-      const data = JSON.parse(msg);
-      const payload = data.payload;
-      const choices = payload.choices;
-      const header = data.header;
-      const code = header.code;
-
-      if (code !== 0) {
-        console.log(payload);
-        return;
-      }
-
-      const status = choices.status;
-      const text = choices.text;
-      const content = text[0].content;
-      if (status !== 2) {
-        answer += content;
-      } else {
-        answer += content;
-        console.log('收到最终结果：', answer);
-        const usage = payload.usage;
-        const temp = usage.text;
-        const totalTokens = temp.total_tokens;
-        console.log('total_tokens:', totalTokens);
-        userHasAnswerIng[FromUserName] = false;
-        userChatHistory[FromUserName].push({
-          Role: 'assistant',
-          Content: answer,
-        });
-        const timeNow2 = Math.floor(Date.now() / 1000);
-        if (timeNow2 - timeNow > 3) {
-          userStashMsg[FromUserName] = answer;
-        }
-        clearTimeout(timeout);
-        resolve();
-      }
-    });
-  });
-
-  const timeoutPromise = new Promise((resolve) => {
-    timeout = setTimeout(() => {
-      userHasAnswerIng[FromUserName] = true;
-      console.log('执行超过4s，提前返回');
-      resolve(
-        formatReply(
-          FromUserName,
-          ToUserName,
-          timeNow,
-          '微信规定要在5s内回复，但是我正在思考中，所以你暂时看到了这条消息。请稍后回复任意文字尝试获取回复。比如数字 1。'
-        )
-      );
-    }, 4000);
-  });
-
-  const result = await Promise.race([done, timeoutPromise]);
-  if (result) {
-    response.status(200).send(result);
-    return;
-  }
-  response.status(200).send(formatReply(FromUserName, ToUserName, timeNow, answer));
-  return
 };
 
 function formatReply(ToUserName, FromUserName, CreateTime, Content) {
@@ -335,55 +216,6 @@ function formatReply(ToUserName, FromUserName, CreateTime, Content) {
     </xml>`;
 }
 
-function genParams(messages) {
-  return {
-    header: {
-      app_id: process.env.APPID,
-    },
-    parameter: {
-      chat: {
-        domain: process.env.SPARK_DOMAIN,
-        temperature: 0.8,
-        top_k: 6,
-        max_tokens: 2048,
-        auditing: 'default',
-      },
-    },
-    payload: {
-      message: {
-        text: messages,
-      },
-    },
-  };
-}
-async function getConnect() {
-  const authUrl = assembleAuthUrl1();
-  const ws = new WebSocket(authUrl);
-  await new Promise((resolve, reject) => {
-    ws.on('open', resolve);
-    ws.on('error', reject);
-  });
-  return ws;
-}
-
-function assembleAuthUrl1() {
-
-const hostUrl = process.env.HOST_URL;
-  const apiKey = process.env.API_KEY;
-  const apiSecret = process.env.API_SECRET;
-  const ul = url.parse(hostUrl);
-  const date = new Date().toUTCString();
-  const signString = `host: ${ul.host}\ndate: ${date}\nGET ${ul.pathname} HTTP/1.1`;
-  const sha = hmacWithShaTobase64('hmac-sha256', signString, apiSecret);
-  const authUrl = `hmac username="${apiKey}", algorithm="hmac-sha256", headers="host date request-line", signature="${sha}"`;
-  const authorization = Buffer.from(authUrl).toString('base64');
-  const v = querystring.stringify({
-    host: ul.host,
-    date: date,
-    authorization: authorization,
-  });
-  return hostUrl + '?' + v;
-}
 
 function hmacWithShaTobase64(algorithm, data, key) {
   const hmac = crypto.createHmac('sha256', key);
